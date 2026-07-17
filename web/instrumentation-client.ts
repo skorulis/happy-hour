abc59ccd5f30dcab7d63110da9b5382f672d2ef4
Alex Skorulis <skorulis@gmail.com>
2026-07-17 14:31:38 +1000

    Add sentry

---
+++ web/instrumentation-client.ts
@@ -0,0 +1,23 @@
+// This file configures the initialization of Sentry on the client.
+// The added config here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://0a387a1fd6e6bad6e0ccacf7a86726ea@o4511748559339520.ingest.de.sentry.io/4511748567924816",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  dataCollection: {
+    // To disable sending user data and HTTP bodies, uncomment the lines below. For more info visit:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#dataCollection
+    // userInfo: false,
+    // httpBodies: [],
+  },
+});
+
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
